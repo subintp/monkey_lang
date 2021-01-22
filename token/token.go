be6c615218746cc4454d458a08d5ddd34711aea4
Subin T P <subin.tp@razorpay.com>
2021-01-23 02:11:01 +0530

    Add token

---
+++ token/token.go
@@ -0,0 +1,37 @@
+package token
+
+// Type is string
+type Type string
+
+// Token in lexer
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+// Possible Tokens
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimeters
+	COMMA     = ","
+	SEMICOLON = ';'
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
